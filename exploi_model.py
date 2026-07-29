cf51f6fe7406688ef94326d8d1ed2c55d904dd46
Mr-zero01 <118440463+Mr-zero01@users.noreply.github.com>
2026-07-29 01:41:13 +0100

    Update exploi_model.py

--- exploi_model.py
+++ exploi_model.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import joblib
+import xgboost as xgb
 
 onehot = joblib.load("fichs/onehot.joblib")
 colonne_hot = joblib.load("fichs/colonne_hot.joblib")
 model = joblib.load("fichs/model_final.joblib")
+model.save_model("fichs/model_final.json")
 
 
 def predict(dt):
+    model = xgb.XGBClassifier()
+    model.load_model("fichs/model_final.json")
     # transform de onehotencoder
     cat_trans = onehot.transform(dt[colonne_hot])
     cols = onehot.get_feature_names_out(colonne_hot)
